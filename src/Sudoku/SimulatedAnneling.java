e49bea425a54f17c70bde0e41d271d4bfdd7c71d
uni-horihori <71215499+uni-horihori@users.noreply.github.com>
2021-05-17 16:47:50 +0800

    update sudoku UI

---
+++ src/Sudoku/SimulatedAnneling.java
@@ -0,0 +1,240 @@
+package Sudoku;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+class Sudoku implements Cloneable{
+    public int size;
+    public int n;
+    public boolean[][] nonFixed;
+    public int[][] board;
+    public Integer[][] nonFixedCells;
+    public Integer[] nonFixedSquares;
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    public Sudoku() {
+    }
+
+    public Sudoku(int size) {
+        this.size = size;
+        n = size * size;
+        nonFixed = new boolean[n][n];
+        board = new int[n][n];
+        nonFixedCells = new Integer[n][];
+    }
+
+    public void read(Scanner input) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                board[i][j] = input.nextInt();
+                nonFixed[i][j] = board[i][j] == 0;
+            }
+        }
+
+        ArrayList<Integer> squares = new ArrayList<>();
+        for (int x = 0; x < size; ++x) {
+            for (int y = 0; y < size; ++y) {
+                // each square
+                ArrayList<Integer> arr = new ArrayList<>();
+
+                boolean[] vis = new boolean[n + 1];
+                int k = 1;
+                for (int i = x * size; i < x * size + size; ++i) {
+                    for (int j = y * size; j < y * size + size; ++j) {
+                        // each fixed cell
+                        if (!nonFixed[i][j]) {
+                            vis[board[i][j]] = true;
+                        } else {
+                            arr.add(i * n + j);
+                            vis[board[i][j]] = false;
+                        }
+                    }
+                }
+
+                for (int i = 0, tx, ty; i < arr.size(); ++i) {
+                    tx = arr.get(i) / n;
+                    ty = arr.get(i) % n;
+                    for (; k <= n; ++k) {
+                        if (!vis[k]) {
+                            vis[k] = true;
+                            board[tx][ty] = k;
+                            break;
+                        }
+                    }
+                }
+
+                if (arr.size() == 1) {
+                    x = arr.get(0) / n;
+                    y = arr.get(0) % n;
+                    nonFixed[x][y] = false;
+                    arr.clear();
+                }
+                nonFixedCells[x * size + y] = arr.toArray(new Integer[0]);
+                if (arr.size() > 0) {
+                    squares.add(x * size + y);
+                }
+            }
+        }
+        nonFixedSquares = squares.toArray(new Integer[0]);
+    }
+
+    public boolean swapCells(double t, double eps) {
+        int a, b, ia, ib, sqr, rand, tmp;
+        Random random = new Random();
+
+        rand = random.nextInt(nonFixedSquares.length);
+        sqr = nonFixedSquares[rand];
+
+        ArrayList<Integer> cnf = new ArrayList<>();
+
+        boolean check;
+        int x, y;
+        for (int cell: nonFixedCells[sqr]) {
+            check = false;
+            x = cell / n;
+            y = cell % n;
+            for (int i = 0; i < n; ++i) {
+                if (x != i && board[x][y] == board[i][y]) {
+                    cnf.add(cell);
+                    check = true;
+                    break;
+                }
+            }
+
+            if(check) continue;
+
+            for (int j = 0; j < n; ++j) {
+                if (y != j && board[x][y] == board[x][j]) {
+                    cnf.add(cell);
+                    break;
+                }
+            }
+        }
+
+        if(cnf.size() <= 0)
+            return false;
+        a = random.nextInt(cnf.size());
+        do {
+            b = random.nextInt(nonFixedCells[sqr].length);
+        } while (b == a);
+
+        ia = cnf.get(a);
+        ib = nonFixedCells[sqr][b];
+
+        int cur_cost, nxt_cost;
+        cur_cost = getCost();
+
+        tmp = board[ia / n][ia % n];
+        board[ia / n][ia % n] = board[ib / n][ib % n];
+        board[ib / n][ib % n] = tmp;
+
+        nxt_cost = getCost();
+
+//        System.out.printf("%d, %d -> %d, %d\n", ia / n, ia % n, ib / n, ib % n);
+        if (nxt_cost <= cur_cost || Math.exp((cur_cost - nxt_cost) / t) >= Math.random()) {
+            return true;
+        }
+
+        // roll back
+        tmp = board[ia / n][ia % n];
+        board[ia / n][ia % n] = board[ib / n][ib % n];
+        board[ib / n][ib % n] = tmp;
+        return false;
+
+    }
+
+    public int getCost() {
+        int cost = 0;
+        boolean[] visx, visy;
+        for (int i = 0; i < n; ++i) {
+            visx = new boolean[n + 1];
+            visy = new boolean[n + 1];
+            for (int j = 0; j < n; ++j) {
+                visx[board[i][j]] = true;
+                visy[board[j][i]] = true;
+            }
+
+            for (int j = 1; j <= n; ++j) {
+                if(!visx[j]) ++cost;
+                if(!visy[j]) ++cost;
+            }
+        }
+        return cost;
+    }
+
+    public void show() {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                System.out.printf("%3d", board[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("=================");
+    }
+
+    public boolean is_fixed(){
+        return nonFixedSquares.length == 0;
+    }
+}
+
+public class SimulatedAnneling {
+    public static double eps = 1e-18;
+    public static double delt = 0.995;
+    public static int T = 500000;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+
+        Sudoku cur;
+        cur = new Sudoku(size);
+        cur.read(scanner);
+
+        int[][] mb;
+        int cost, min;
+        double t;
+
+        System.out.println("round " + 0 + ": " + cur.getCost());
+        cur.show();
+        int cnt = 0;
+        min = cur.getCost();
+        mb = cur.board.clone();
+        do {
+            cur.board = mb.clone();
+            cost = min;
+            t = T;
+            boolean ret;
+            while (t >= eps && cost > 0) {
+                ret = cur.swapCells(t, eps);
+//                System.out.println(t + ", " + cur.getCost() + ": " + (ret ? "swap success" : "swap fail"));
+                if(ret) {
+                    cost = cur.getCost();
+                    if(cost <= min){
+                        if(cost < min){
+                            System.out.println("round " + cnt + ": " + min);
+                            cur.show();
+                        }
+                        min = cost;
+                        mb = cur.board.clone();
+                    }
+                }
+                t *= delt;
+            }
+            ++cnt;
+            if(cnt % 200 == 0){
+                System.out.println("round " + cnt + ": " + min);
+            }
+
+        } while (cur.getCost() > 0);
+
+        System.out.println("The answer:");
+        cur.show();
+    }
+
+
+}
--- src/Sudoku/view/LayoutController.java
+++ src/Sudoku/view/LayoutController.java
@@ -1,9 +1,12 @@
 package Sudoku.view;
 
+import com.sun.javafx.collections.ObservableIntegerArrayImpl;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableIntegerArray;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 
@@ -13,11 +16,20 @@ public class LayoutController {
     @FXML
     private GridPane sudokuTextContainer;
 
+    @FXML
+    private ChoiceBox<String> preset;
+
     private static TextField[][] sudokuCells = new TextField[9][9];
+    private boolean playerCtrl = false;
+
+
+    private int[][] sudokuBoard;
+    private boolean[][] blocked;
 
     @FXML
     private void initialize(){
         sudokuTextContainer.getStyleClass().add("cells-container");
+
         for (int rowIndex = 0; rowIndex < 9; rowIndex++) {
             for (int columnIndex = 0; columnIndex < 9; columnIndex++) {
                 sudokuCells[rowIndex][columnIndex] = new TextField();
@@ -25,17 +37,92 @@ public class LayoutController {
                 sudokuTextContainer.getChildren().add(sudokuCells[rowIndex][columnIndex]);
 
                 sudokuCells[rowIndex][columnIndex].getStyleClass().add("cell");
-
                 if(columnIndex==2||columnIndex==5){
                     sudokuCells[rowIndex][columnIndex].getStyleClass().add("border-right");
                 }
+                if(rowIndex == 3 ||rowIndex==6){
+                    sudokuCells[rowIndex][columnIndex].getStyleClass().add("border-top");
+                    if(columnIndex==2||columnIndex==5){
+                        sudokuCells[rowIndex][columnIndex].getStyleClass().add("border-top-right");
+                    }
+                }
 
+                TextField current = sudokuCells[rowIndex][columnIndex];
+                int finalRowIndex = rowIndex;
+                int finalColumnIndex = columnIndex;
+                current.textProperty().addListener((observable, oldVal, newVal) -> {
+                    if(current.getText().length()>1){
+                        current.setText("");
+                    }
+                    else if(!isInputValid(current.getText())){
+                        current.setText("");
+                    }
+                });
 
             }
-
-
         }
+
+        preset.getItems().addAll("EASY","NORMAL","HARD");
 
 
     }
+
+    @FXML
+    private void selectPreset(){
+        int selectedIndex = preset.getSelectionModel().selectedIndexProperty().intValue();
+        int[][] inSudoku = new int[9][9];
+        if(selectedIndex==0){
+            //easy
+
+        }else if(selectedIndex == 1){
+            //normal
+
+        }else if(selectedIndex ==2){
+            //hard
+        }
+
+        for (int row = 0; row < 9; row++) {
+            for (int column = 0; column < 9; column++) {
+                if(inSudoku[row][column]!=0){
+                    sudokuCells[row][column].setText(String.valueOf(inSudoku[row][column]));
+                    sudokuCells[row][column].setEditable(false);
+                }
+                else{
+                    sudokuCells[row][column].setText("");
+                    sudokuCells[row][column].setEditable(true);
+                }
+
+            }
+        }
+
+    }
+
+    private boolean isInputValid(String input) {
+        int checkInput;
+        try {
+            checkInput = Integer.parseInt(input);
+            if (checkInput <= 0 || checkInput > 9) {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    private void showBoard(){
+//        int[][] newBoard =
+
+
+    }
+
+    private void setUserEdit(){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                sudokuCells[i][j].setEditable(playerCtrl);
+            }
+        }
+    }
+
+
 }
