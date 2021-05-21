26a1ca183adff36586e082c60d063ee40b144de3
BravoChaoS <43561142+BravoChaoS@users.noreply.github.com>
2021-05-21 10:51:44 +0800

    comment

--- src/module/magic_square/MagicSquare.java
+++ src/module/magic_square/MagicSquare.java
@@ -25,12 +25,16 @@ public class MagicSquare {
     public static final double T = 1e4;
     public static final int S = 500;
 
-
     public static final double EPS2 = 1e-16;
     public static final double DELT2 = 0.95;
     public static final double T2 = 1e3;
     public static final int S2 = 1;
 
+    /**
+     * Instantiates a new Magic square.
+     *
+     * @param n the magic square size
+     */
     public MagicSquare(int n) {
         this.n = n;
         this.cnst = new int[n][n];
@@ -56,160 +60,13 @@ public class MagicSquare {
             mutates2.add(new MutateS21());
     }
 
-    public void readMs(String fn) throws IOException {
-        FileInputStream fileInputStream = new FileInputStream(fn);
 
-        Scanner fileScanner = new Scanner(fileInputStream);
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                cnst[i][j] = fileScanner.nextInt();
-            }
-        }
-    }
-
-    public int[][] boardCopy() {
-        int[][] nxt = new int[n][];
-        for (int i = 0; i < n; ++i) {
-            nxt[i] = board[i].clone();
-        }
-        return nxt;
-    }
-
-    public int[][] boardCopy(int[][] b) {
-        int[][] nxt = new int[n][];
-        for (int i = 0; i < n; ++i) {
-            nxt[i] = b[i].clone();
-        }
-        return nxt;
-    }
-
-    public void fill() {
-        boolean[] vis = new boolean[n * n + 1];
-        for (int[] row : cnst) {
-            for (int x : row) {
-                vis[x] = true;
-            }
-        }
-
-        int cnt = 1;
-        while (cnt < n * n + 1 && vis[cnt])
-            ++cnt;
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (cnst[i][j] == 0) {
-                    board[i][j] = cnt++;
-                    while (cnt < n * n + 1 && vis[cnt])
-                        ++cnt;
-                } else {
-                    board[i][j] = cnst[i][j];
-                }
-            }
-        }
-    }
-
-    public void show() {
-        for (int[] row : board) {
-            for (int x : row) {
-                System.out.printf("%6d", x);
-            }
-            System.out.println();
-        }
-    }
-
-    public int evl1(int[][] b) {
-        int error = 0, sum;
-
-        for (int i = 0; i < n; ++i) {
-            sum = 0;
-            for (int j = 0; j < n; ++j) {
-                sum += b[i][j];
-            }
-            error += Math.abs(sum - mn);
-
-            sum = 0;
-            for (int j = 0; j < n; ++j) {
-                sum += b[j][i];
-            }
-            error += Math.abs(sum - mn);
-        }
-
-        return error;
-    }
-
-    public int evl2(int[][] b) {
-        int error = 0, sum;
-
-        sum = 0;
-        for (int i = 0; i < n; ++i) {
-            sum += b[i][i];
-        }
-        error += Math.abs(sum - mn);
-
-        sum = 0;
-        for (int i = 0; i < n; ++i) {
-            sum += b[i][n - i - 1];
-        }
-        error += Math.abs(sum - mn);
-
-        return error;
-    }
-
-    public int evl(int[][] b) {
-        return evl1(b) + evl2(b);
-    }
-
-    public void algoInit() {
-        t = T;
-//        board = boardCopy(min_brd);
-    }
-
-    public void algoInit2() {
-        t = T2;
-//        board = boardCopy(min_brd);
-    }
-
-    public int[][] getNext() {
-        Random random = new Random();
-        int chs = random.nextInt(mutates.size());
-
-        return mutates.get(chs).mutate(this);
-    }
-
-    public int[][] getNext2() {
-        Random random = new Random();
-        int chs = random.nextInt(mutates2.size());
-
-        return mutates2.get(chs).mutate(this);
-    }
-
-    public boolean getNextGeneration() {
-        int[][] nxt = getNext();
-        int nxt_err = evl1(nxt), cur_err = evl1(board);
-
-        if (nxt_err <= cur_err || Math.exp((cur_err - nxt_err) / t) >= Math.random()) {
-            board = nxt;
-//            if (nxt_err < 50 * n && Math.random() < 0.001)
-//                board = local1.mutate(this);
-            return true;
-        }
-
-        return false;
-    }
-
-    public boolean getNextGeneration2() {
-        int[][] nxt = getNext2();
-        int nxt_err = evl2(nxt), cur_err = evl2(board);
-
-        if (nxt_err <= cur_err || Math.exp((cur_err - nxt_err) / t) >= Math.random()) {
-            board = nxt;
-            return true;
-        }
-
-        return false;
-    }
-
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     * @throws Exception the exception
+     */
     public static void main(String[] args) throws Exception {
         MagicSquare ms = new MagicSquare(20);
 
@@ -221,9 +78,11 @@ public class MagicSquare {
         double[] tot_times = new double[rnd];
 
         for (int it = 0; it < rnd; ++it) {
+            ms.fill();
+
+            // step 1
             System.out.println("new test: " + it + "\n" +
                     "step1:=======");
-            ms.fill();
             long t1 = System.currentTimeMillis();
 
             int gen = 0;
@@ -248,7 +107,6 @@ public class MagicSquare {
             long t2 = System.currentTimeMillis();
             times1[it] = t2 - t1;
 
-//        ms.board = ms.min_brd;
             System.out.printf("time: %dms, gen: %d, cnt: %d\n" +
                     "evl: %d, evl1: %d, evl2: %d\n", t2 - t1, gen, cnt, ms.evl(ms.board), ms.evl1(ms.board), ms.evl2(ms.board));
 
@@ -277,12 +135,13 @@ public class MagicSquare {
             t2 = System.currentTimeMillis();
             times2[it] += t2 - t1;
 
-//        ms.board = ms.min_brd;
             System.out.printf("time: %dms, gen: %d, cnt: %d\n" +
                     "evl: %d, evl1: %d, evl2: %d\n", t2 - t1, gen, cnt, ms.evl(ms.board), ms.evl1(ms.board), ms.evl2(ms.board));
 
             tot_times[it] = times1[it] + times2[it];
         }
+
+        // output result
         long sum;
         double dev;
 
@@ -301,4 +160,226 @@ public class MagicSquare {
 
         ms.show();
     }
+
+    /**
+     * Read ms.
+     *
+     * @param fn the file name
+     * @throws IOException the io exception
+     */
+    public void readMs(String fn) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(fn);
+
+        Scanner fileScanner = new Scanner(fileInputStream);
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                cnst[i][j] = fileScanner.nextInt();
+            }
+        }
+    }
+
+    /**
+     * Board copy int [ ] [ ].
+     *
+     * @return a copy of ms.board
+     */
+    public int[][] boardCopy() {
+        int[][] nxt = new int[n][];
+        for (int i = 0; i < n; ++i) {
+            nxt[i] = board[i].clone();
+        }
+        return nxt;
+    }
+
+    /**
+     * Board copy int [ ] [ ].
+     *
+     * @param b the copied board
+     * @return the board
+     */
+    public int[][] boardCopy(int[][] b) {
+        int[][] nxt = new int[n][];
+        for (int i = 0; i < n; ++i) {
+            nxt[i] = b[i].clone();
+        }
+        return nxt;
+    }
+
+    /**
+     * Fill a initial board with constraints
+     */
+    public void fill() {
+        boolean[] vis = new boolean[n * n + 1];
+        for (int[] row : cnst) {
+            for (int x : row) {
+                vis[x] = true;
+            }
+        }
+
+        int cnt = 1;
+        while (cnt < n * n + 1 && vis[cnt])
+            ++cnt;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (cnst[i][j] == 0) {
+                    board[i][j] = cnt++;
+                    while (cnt < n * n + 1 && vis[cnt])
+                        ++cnt;
+                } else {
+                    board[i][j] = cnst[i][j];
+                }
+            }
+        }
+    }
+
+    /**
+     * Show the board
+     */
+    public void show() {
+        for (int[] row : board) {
+            for (int x : row) {
+                System.out.printf("%6d", x);
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Evl 1 int.
+     *
+     * @param b the board
+     * @return the row and column error
+     */
+    public int evl1(int[][] b) {
+        int error = 0, sum;
+
+        for (int i = 0; i < n; ++i) {
+            sum = 0;
+            for (int j = 0; j < n; ++j) {
+                sum += b[i][j];
+            }
+            error += Math.abs(sum - mn);
+
+            sum = 0;
+            for (int j = 0; j < n; ++j) {
+                sum += b[j][i];
+            }
+            error += Math.abs(sum - mn);
+        }
+
+        return error;
+    }
+
+    /**
+     * Evl 2 int.
+     *
+     * @param b the board
+     * @return the error of diagonals
+     */
+    public int evl2(int[][] b) {
+        int error = 0, sum;
+
+        sum = 0;
+        for (int i = 0; i < n; ++i) {
+            sum += b[i][i];
+        }
+        error += Math.abs(sum - mn);
+
+        sum = 0;
+        for (int i = 0; i < n; ++i) {
+            sum += b[i][n - i - 1];
+        }
+        error += Math.abs(sum - mn);
+
+        return error;
+    }
+
+    /**
+     * Evl int.
+     *
+     * @param b the board
+     * @return the error of the board
+     */
+    public int evl(int[][] b) {
+        return evl1(b) + evl2(b);
+    }
+
+    /**
+     * Init of step 1
+     */
+    public void algoInit() {
+        t = T;
+//        board = boardCopy(min_brd);
+    }
+
+    /**
+     * Init of step 2
+     */
+    public void algoInit2() {
+        t = T2;
+//        board = boardCopy(min_brd);
+    }
+
+    /**
+     * Get next board in step 1, the original board won't change
+     *
+     * @return a generated board of step 1
+     */
+    public int[][] getNext() {
+        Random random = new Random();
+        int chs = random.nextInt(mutates.size());
+
+        return mutates.get(chs).mutate(this);
+    }
+
+    /**
+     * Get next board in step 2, the original board won't change
+     *
+     * @return a generated board of step 2
+     */
+    public int[][] getNext2() {
+        Random random = new Random();
+        int chs = random.nextInt(mutates2.size());
+
+        return mutates2.get(chs).mutate(this);
+    }
+
+    /**
+     * Gets next generation in step 1.
+     *
+     * @return useless
+     */
+    public boolean getNextGeneration() {
+        int[][] nxt = getNext();
+        int nxt_err = evl1(nxt), cur_err = evl1(board);
+
+        if (nxt_err <= cur_err || Math.exp((cur_err - nxt_err) / t) >= Math.random()) {
+            board = nxt;
+//            if (nxt_err < 50 * n && Math.random() < 0.001)
+//                board = local1.mutate(this);
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Gets next generation in step 2.
+     *
+     * @return  useless
+     */
+    public boolean getNextGeneration2() {
+        int[][] nxt = getNext2();
+        int nxt_err = evl2(nxt), cur_err = evl2(board);
+
+        if (nxt_err <= cur_err || Math.exp((cur_err - nxt_err) / t) >= Math.random()) {
+            board = nxt;
+            return true;
+        }
+
+        return false;
+    }
+
 }
